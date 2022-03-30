80404e85935bfed4b6991247b80da18ee7221de5
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2022-03-31 01:21:45 +0900

    Changed some logic

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ const getData = async function (url) {
     const res = await fetch(url);
     return await res.text();
 };
+
+function base64Encode(...parts) {
+    return new Promise(resolve => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const offset = reader.result.indexOf(",") + 1;
+            resolve(reader.result.slice(offset));
+        };
+        reader.readAsDataURL(new Blob(parts));
+    });
+}
