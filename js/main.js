3c78aba22c9399444f10c25bbc975989d728c398
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2022-04-10 00:12:07 +0900

    Done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const getData = async function (url) {
-    const res = await fetch(url);
-    return await res.text();
-};
-
-function base64Encode(...parts) {
-    return new Promise(resolve => {
-        const reader = new FileReader();
-        reader.onload = () => {
-            const offset = reader.result.indexOf(",") + 1;
-            resolve(reader.result.slice(offset));
-        };
-        reader.readAsDataURL(new Blob(parts));
-    });
-}
-
 let uploadURL;
 var booksRegistered = [];
 const openbdURL = "https://api.openbd.jp/v1/get?isbn=";
